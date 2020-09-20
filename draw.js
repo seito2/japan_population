3af95904b2550ba3f75c1af418284ced72a130c8
seito2 <seito1201@gmail.com>
2020-09-21 00:01:50 +0900

    [add] load geojson in tellusos

---
+++ draw.js
@@ -0,0 +1,52 @@
+function drawGeo ( url ) {
+  $.ajax({
+    url: url,
+    type: "GET",
+    contentType: "application/json",
+    headers: {
+        "X-CSRF-TOKEN": UserToken,
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "X-Requested-With": "jquery"
+  },
+  success: function (data) {
+      console.log(data);
+      L.geoJSON(data, {
+        style: function (feature) {
+          return { 
+            fillColor: getColor(feature.properties.JINKO_LEVEL), 
+            fillOpacity: 0.8,
+            opacity: 0
+          };
+        }
+      }).addTo(map);
+    }
+  });
+}
+
+function getColor(level) {
+  switch (level) {
+    case 0:
+      return "#ffffcc";
+
+    case 1:
+      return "#fed976";
+    
+    case 2:
+      return "#fd8c3c";
+
+    case 3:
+      return "#e2191c";
+
+    case 4:
+      return "#800026";
+      
+    default:
+      return "#ffffcc";
+  }
+
+}
+
+for(var i = 1; i < 47; ++i) {
+   drawGeo("https://cdn.jsdelivr.net/gh/seito2/japan_population@latest/" + i + ".geojson");
+}
